374ba81bee4ea1ef3facb002c8a1262ccf8e07ff
Technotraxx <114928644+Technotraxx@users.noreply.github.com>
2024-06-05 15:03:50 +0200

    Update anthropic_claude3.py

--- anthropic_claude3.py
+++ anthropic_claude3.py
@@ -1,16 +1,19 @@
-import anthropic
+import openai
 
-class AnthropicClaude3API:
-    def __init__(self, api_key, model_name="claude-3-opus-20240229"):
+class OpenAIGPT4oAPI:
+    def __init__(self, api_key, model_name="gpt-4o"):
         self.api_key = api_key
         self.model_name = model_name
-        self.client = anthropic.Anthropic(api_key=api_key)
+        openai.api_key = self.api_key
 
-    def generate_completion(self, prompt, max_tokens=2048, temperature=0.9):
-        response = self.client.messages.create(
+    def generate_completion(self, messages, temperature=0.5, max_tokens=1500, top_p=1, frequency_penalty=0, presence_penalty=0):
+        response = openai.ChatCompletion.create(
             model=self.model_name,
-            max_tokens=max_tokens,
+            messages=messages,
             temperature=temperature,
-            messages=[{"role": 'user', "content": prompt}]
+            max_tokens=max_tokens,
+            top_p=top_p,
+            frequency_penalty=frequency_penalty,
+            presence_penalty=presence_penalty
         )
         return response
