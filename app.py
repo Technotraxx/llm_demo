6dd7c9eb056aa24eb9f24aff335fe02cc200fdd1
Technotraxx <114928644+Technotraxx@users.noreply.github.com>
2024-06-05 15:04:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 import streamlit as st
 from pypdf import PdfReader
-from anthropic import Anthropic
+from anthropic_claude3 import AnthropicClaude3API
+from openai_gpt4o import OpenAIGPT4oAPI
+from config_anthropic_claude3 import DEFAULT_MODEL_NAME as CLAUDE_MODEL
+from config_openai_gpt4o import DEFAULT_MODEL_NAME as GPT4O_MODEL
 from templates import prompt_templates
 from utils import save_text, save_csv, save_doc, save_xls, send_email, reload_page, generate_unique_filename
 from layout import create_sidebar, create_main_area, create_output_area
 
 # Initialize session state variables
+if "api_provider" not in st.session_state:
+    st.session_state.api_provider = "Anthropic Claude 3"
 if "model_name" not in st.session_state:
-    st.session_state.model_name = "Claude 3 Opus"
+    st.session_state.model_name = CLAUDE_MODEL if st.session_state.api_provider == "Anthropic Claude 3" else GPT4O_MODEL
 if "max_tokens" not in st.session_state:
-    st.session_state.max_tokens = 256
+    st.session_state.max_tokens = 2048 if st.session_state.api_provider == "Anthropic Claude 3" else 1500
 if "temperature" not in st.session_state:
-    st.session_state.temperature = 0.9
+    st.session_state.temperature = 0.9 if st.session_state.api_provider == "Anthropic Claude 3" else 0.5
 if "prompt" not in st.session_state:
     st.session_state.prompt = prompt_templates["Default Template"]
 if "word_count" not in st.session_state:
@@ -39,54 +44,53 @@ if "text" in st.session_state and st.session_state.text:
     with st.expander(f"Extracted Text (Word count: {st.session_state.word_count}):"):
         st.write(st.session_state.text[:2000])  # Display the first 2000 characters
 
-api_key = st.secrets["anthropic_api_key"]
-client = Anthropic(api_key=api_key)
+api_provider = st.selectbox("Choose API Provider", ["Anthropic Claude 3", "OpenAI GPT-4o"], key="api_provider")
 
-model_options = {
-    "Claude 3 Opus": "claude-3-opus-20240229",
-    "Claude 3 Sonnet": "claude-3-sonnet-20240229",
-    "Claude 3 Haiku": "claude-3-haiku-20240307"
-}
-model_name = st.session_state.model_name
-max_tokens = st.session_state.max_tokens
-temperature = st.session_state.temperature
-
-# Dropdown menu for prompt templates
-template_name = st.selectbox("Choose a prompt template", list(prompt_templates.keys()))
-
-# Set the prompt based on the selected template
-if st.button("Use Template"):
-    st.session_state.prompt = prompt_templates[template_name].replace("{text}", "{text}")
+if api_provider == "Anthropic Claude 3":
+    api_key = st.secrets["anthropic_api_key"]
+    api_client = AnthropicClaude3API(api_key=api_key, model_name=st.session_state.model_name)
+elif api_provider == "OpenAI GPT-4o":
+    api_key = st.secrets["openai_api_key"]
+    api_client = OpenAIGPT4oAPI(api_key=api_key, model_name=st.session_state.model_name)
 
 # Editable text area for the prompt
 prompt = st.text_area("Edit the prompt", value=st.session_state.prompt, height=300, key="prompt_text_area")
 
-def get_completion(client, prompt, model_name, max_tokens, temperature):
-    try:
-        response = client.messages.create(
-            model=model_name,
-            max_tokens=max_tokens,
-            temperature=temperature,
-            messages=[{
-                "role": 'user', "content":  prompt
-            }]
+def get_completion(messages, model_name, max_tokens, temperature, top_p, frequency_penalty, presence_penalty):
+    if st.session_state.api_provider == "Anthropic Claude 3":
+        response = api_client.generate_completion(
+            prompt=messages,
+            max_tokens=max_tokens
         )
-        return response.content[0].text
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-        return ""
+        return response.messages[0].text
+    elif st.session_state.api_provider == "OpenAI GPT-4o":
+        response = api_client.generate_completion(
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            top_p=top_p,
+            frequency_penalty=frequency_penalty,
+            presence_penalty=presence_penalty
+        )
+        return response.choices[0].message["content"]
 
 if st.button("Generate Summary"):
-    # Update the session state with the edited prompt
     st.session_state.prompt = st.session_state.prompt_text_area
-
-    # Replacing {text} in the user-edited prompt
     prompt_with_text = st.session_state.prompt.replace("{text}", st.session_state.text)
-    completion = get_completion(client,
-        prompt_with_text, model_options[model_name], max_tokens, temperature
+    messages = [
+        {"role": "system", "content": [{"type": "text", "text": "You are a helpful assistant."}]},
+        {"role": "user", "content": [{"type": "text", "text": prompt_with_text}]}
+    ]
+    completion = get_completion(
+        messages=prompt_with_text if st.session_state.api_provider == "Anthropic Claude 3" else messages,
+        model_name=st.session_state.model_name,
+        max_tokens=st.session_state.max_tokens,
+        temperature=st.session_state.temperature,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
-    if completion:
-        st.session_state.summary = completion
+    st.session_state.summary = completion
 
 # Create output area
 create_output_area(st.session_state.summary)
