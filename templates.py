45d2c9a981a6ece37ecaeaffd444cc17241b7230
Technotraxx <114928644+Technotraxx@users.noreply.github.com>
2024-06-05 13:19:38 +0200

    Update templates.py

--- templates.py
+++ templates.py
@@ -1,11 +1,5 @@
 prompt_templates = {
-    "Default Template": """Here is an article: <paper>{text}</paper>
-
-    Please do the following:
-    1. Summarize the abstract at a kindergarten reading level. (In <kindergarten_abstract> tags.)
-    2. Write the Methods section as a recipe from the Moosewood Cookbook. (In <moosewood_methods> tags.)
-    3. Compose a short poem epistolizing the results in the style of Homer. (In <homer_results> tags.)
-    """,
+    "Default Template": "Zusammenfassung in 5 Wörtern!\n\n{text}",
     "Template A": "<input>{text}</input>\n\nCreate a short and memorable summary of the previous text input.\n1. One sentence summary\n2. One paragraph summary\n3. 5 important concepts or definitions or theories",
     "Template B": "Here is an academic paper: <paper>{text}</paper>\n\nPlease do the following:\n1. Summarize the abstract at a kindergarten reading level. (In <kindergarten_abstract> tags.)\n2. Write the Methods section as a recipe from the Moosewood Cookbook. (In <moosewood_methods> tags.)\n\3. Compose a short poem epistolizing the results in the style of Homer. (In <homer_results> tags.)",
     "Template C": "Prompt text for template C",
