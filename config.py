e6b1d43bef653349de1b2728bb99c0a858df6c40
Technotraxx <114928644+Technotraxx@users.noreply.github.com>
2024-06-05 20:36:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,9 @@ def initialize_session_state():
     if "settings" not in st.session_state:
         st.session_state.settings = {
             "api_provider": "Anthropic Claude 3",
+            "api_provider_index": 1,
             "model_name": "claude-3-opus-20240229",
+            "model_name_index": 0,
             "max_tokens": 512,
             "temperature": 0.7,
             "prompt": prompt_templates["Default Template"]
@@ -22,7 +24,9 @@ def initialize_session_state():
 def reset_session_state():
     st.session_state.settings = {
         "api_provider": "Anthropic Claude 3",
+        "api_provider_index": 1,
         "model_name": "claude-3-opus-20240229",
+        "model_name_index": 0,
         "max_tokens": 512,
         "temperature": 0.7,
         "prompt": prompt_templates["Default Template"]
@@ -33,19 +37,4 @@ def reset_session_state():
         "text": ""
     }
     st.session_state.template_name = "Default Template"
-    st.session_state.api_provider = "Anthropic Claude 3"
-    st.session_state.model_name = "claude-3-opus-20240229"
     st.experimental_rerun()  # Reload the page to reset the states
-
-
-MODEL_OPTIONS = {
-    "OpenAI GPT-4o": ["gpt-4o", "gpt-3.5-turbo-16k"],
-    "Anthropic Claude 3": ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307"],
-    "Google Gemini": ["gemini-1.5-pro", "gemini-1.5-flash", "gemini-1.0-pro", "gemini-pro-vision"]
-}
-
-def filter_models(api_choice):
-    model_options = MODEL_OPTIONS.get(api_choice, [])
-    if st.session_state.settings["model_name"] not in model_options:
-        st.session_state.settings["model_name"] = model_options[0] if model_options else None 
-    return model_options
