3babf24e0c38b1c9626c32bbda035dd30ee64677
Technotraxx <114928644+Technotraxx@users.noreply.github.com>
2024-06-05 11:12:35 +0200

    Update templates.py

--- templates.py
+++ templates.py
@@ -1,6 +1,6 @@
 prompt_templates = {
     "Template A": "<input>{text}</input>\n\nCreate a short and memorable summary of the previous text input.\n1. One sentence summary\n2. One paragraph summary\n3. 5 important concepts or definitions or theories",
-    "Template B": "Prompt text for template B",
+    "Template B": "Here is an academic paper: <paper>{text}</paper>\n\nPlease do the following:\n1. Summarize the abstract at a kindergarten reading level. (In <kindergarten_abstract> tags.)\n2. Write the Methods section as a recipe from the Moosewood Cookbook. (In <moosewood_methods> tags.)\n\3. Compose a short poem epistolizing the results in the style of Homer. (In <homer_results> tags.)",
     "Template C": "Prompt text for template C",
     "Template D": "Prompt text for template D",
     "Template E": "Prompt text for template E"
