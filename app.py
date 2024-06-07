78fd31c1a23e0bd9c7de95ed7a22ed6ba009ede3
Technotraxx <114928644+Technotraxx@users.noreply.github.com>
2024-06-07 13:25:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -59,9 +59,11 @@ if youtube_input and (submit_youtube or st.session_state.get("youtube_input_chan
     if video_id:
         languages = list_available_transcripts(video_id)
         if languages:
-            selected_language = st.selectbox("Select Language", languages, key=f"language_select_{video_id}")
-        if selected_language:
-            text, word_count = load_youtube_transcript(video_id, [selected_language])  # Pass the video_id and selected_language
+            unique_key = f"language_select_{video_id}_{uuid.uuid4()}"
+            selected_language = st.selectbox("Select Language", languages, key=unique_key)
+            st.session_state.selected_language = selected_language
+        if st.session_state.selected_language:
+            text, word_count = load_youtube_transcript(video_id, [st.session_state.selected_language])  # Pass the video_id and selected_language
             if word_count == 0:
                 st.error(text)
             else:
