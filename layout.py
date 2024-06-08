7f0682bd13532ac4e9b2032b341a2feacbe305f4
Technotraxx <114928644+Technotraxx@users.noreply.github.com>
2024-06-09 01:37:44 +0200

    Update layout.py

--- layout.py
+++ layout.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 from utils import (save_file, send_email, reload_page, generate_unique_filename, load_pdf, load_docx,
                    load_txt, load_csv, load_url)
-from youtube_api import process_youtube_input
+from youtube_api import process_youtube_input  # Importiere die neue Datei ohne Sprachauswahl
 
 def create_sidebar():
     st.sidebar.title("Settings")
